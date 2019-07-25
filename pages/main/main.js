59eaedd0291f2b5ac45021d2703950d3fb929589
wys <1920303146@qq.com>
2019-07-25 16:24:08 +0800

    配置config后上传

---
+++ pages/main/main.js
@@ -0,0 +1,73 @@
+// pages/main/main.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+      imgList:[
+        { url: "https://offlintab.firefoxchina.cn/static/img/search/baidu_web.png",text:"百度"},
+        { url: "https://offlintab.firefoxchina.cn/static/img/search/taobao_shopping-446.png", text: "淘宝网" },
+        { url: "https://offlintab.firefoxchina.cn/static/img/search/google_web-444.png" ,text: "谷歌" },
+        { url: "https://img.58cdn.com.cn/logo/58/252_84/logo-o.png?v=2",text:"58同城"},
+        { url: "https://b.appsimg.com/upload/momin/2019/02/21/133/1550737570483.png",text:"唯品会"}
+      ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
